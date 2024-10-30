a88cf09ac73d8cdcee78ed6dbfb187d6ccce0c86
uuuunseo <jdbstjoo@gmail.com>
2024-10-31 00:08:51 +0900

    :lipstick: :: CheckList - plusButton 추가

--- CheckListt/src/App.js
+++ CheckListt/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
 import ListItem from "./components/ListItem";
 
 function App() {
     const todos = ["할 일", "하자", "진짜", "한다", "진짜로"]
     return( 
         <View style={styles.total_container}>
-            <Text style={styles.title}>체크리스트</Text>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>체크리스트</Text>
+
+                <TouchableOpacity style={styles.plusButton}>
+                    <Text> + </Text>
+                </TouchableOpacity>
+            </View>
+
             <ScrollView contentContainerStyle={styles.checkListContainer}>
                 {todos.map((todo, index) => (
                     <ListItem key={index} title={todo}/>
@@ -21,13 +28,27 @@ export default App;
 const styles = StyleSheet.create({
     totalContainer:{
         flex: 1,
-        backgroundColor: '#fff'
+        backgroundColor: '#fff',
+        marginHorizontal: 24
+    },
+    titleContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 30,
+        marginHorizontal: 16
     },
     title: {
         fontSize: 28,
-        marginTop: 30,
         fontWeight: 700,
-        marginLeft: 24
+    },
+    plusButton: {
+        width: 24, 
+        height: 24, 
+        backgroundColor: '#AAD1E7',
+        alignItems: 'center', 
+        justifyContent: 'center'
     },
     checkListContainer: {
         marginTop: 10
