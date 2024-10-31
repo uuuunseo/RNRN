ddcf425910582067ddedac9f967af85b5057374d
uuuunseo <jdbstjoo@gmail.com>
2024-10-31 23:22:50 +0900

    :lipstick: :: 할 일 입력 Modal 추가

--- CheckListt/src/App.js
+++ CheckListt/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import ListItem from "./components/ListItem";
 
 function App() {
     const todos = ["할 일", "하자", "진짜", "한다", "진짜로"]
+    const [newTask, setNewTask] = useState('');
+    const [tasks, setTasks] = useState(todos);
+
+    // 할 일 추가 (todos에 newTask로 작성한 String이 append되고, tasks가 update 되면 리스트 다시 불러오기?)
+    const addTask = () => {
+        if(newTask) {
+
+        }
+    }
+
     return( 
         <View style={styles.total_container}>
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>체크리스트</Text>
 
                 <TouchableOpacity style={styles.plusButton}>
-                    <Text> + </Text>
+                    <Text style={{color: '#fff'}}> + </Text>
                 </TouchableOpacity>
             </View>
 
@@ -36,7 +47,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
-        marginTop: 30,
+        marginTop: 50,
         marginHorizontal: 16
     },
     title: {
---
+++ CheckListt/src/components/AddInputTask.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { TextInput } from "../../node_modules/react-native/types/index";
+
+const AddInputTask = () => {
+    return(
+        <TextInput placeholder="할 일을 추가하자ㅏㅏ"/>
+    )
+}
