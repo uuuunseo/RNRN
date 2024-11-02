5f460d062a8d0ffe332f54c08d2e682ad8c87d8e
uuuunseo <jdbstjoo@gmail.com>
2024-11-02 13:46:55 +0900

    :lipstick: :: CheckList - TextInput

--- CheckListt/src/App.js
+++ CheckListt/src/App.js
@@ -1,34 +1,33 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, ScrollView, TouchableOpacity } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import { View, StyleSheet, Text, ScrollView, TouchableOpacity, TextInput } from "react-native";
 import ListItem from "./components/ListItem";
 
 function App() {
-    const todos = ["할 일", "하자", "진짜", "한다", "진짜로"]
-    const [newTask, setNewTask] = useState('');
-    const [tasks, setTasks] = useState(todos);
+    const [task, setTask] = useState('');
 
-    // 할 일 추가 (todos에 newTask로 작성한 String이 append되고, tasks가 update 되면 리스트 다시 불러오기?)
-    const addTask = () => {
-        if(newTask) {
-
-        }
+    const onChangeTask = (inputTask) => {
+        setTask(inputTask)
     }
-
+    
     return( 
         <View style={styles.total_container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>체크리스트</Text>
+            <Text style={styles.title}>체크리스트</Text>
 
-                <TouchableOpacity style={styles.plusButton}>
+            <View style={styles.inputContainer}>
+            <TextInput
+                onChangeText={onChangeTask}
+                value={task}
+                placeholder="할 일을 입력하세요.."
+                style={{height: 40, borderColor: 'black', borderWidth: 1, paddingHorizontal: 8, cornerRadius: 8, flex: 9}}
+            />
+
+            <TouchableOpacity style={styles.plusButton}>
                     <Text style={{color: '#fff'}}> + </Text>
-                </TouchableOpacity>
+            </TouchableOpacity>
             </View>
 
             <ScrollView contentContainerStyle={styles.checkListContainer}>
-                {todos.map((todo, index) => (
-                    <ListItem key={index} title={todo}/>
-                ))}
+                
             </ScrollView>
         </View>
     );
@@ -42,21 +41,23 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         marginHorizontal: 24
     },
-    titleContainer: {
+    inputContainer: {
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
+        justifyContent: 'flex-start',
         marginTop: 50,
         marginHorizontal: 16
     },
     title: {
         fontSize: 28,
         fontWeight: 700,
+        marginTop: 30,
+        marginLeft: 10
     },
     plusButton: {
-        width: 24, 
-        height: 24, 
+        width: 40, 
+        height: 40, 
         backgroundColor: '#AAD1E7',
         alignItems: 'center', 
         justifyContent: 'center'
