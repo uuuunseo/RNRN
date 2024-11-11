6c983946dfb0d75f5fdda8ab82bb1e9afa412b3b
uuuunseo <jdbstjoo@gmail.com>
2024-11-12 00:38:30 +0900

    :sparkles: :: RealStoryView - 수정하기 페이지 버튼 선택 시 텍스트 바뀌도록 구현

--- RealStoryView/src/screens/EditStoryView.js
+++ RealStoryView/src/screens/EditStoryView.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import { WithLocalSvg } from 'react-native-svg/css';
-import { View, SafeAreaView, TouchableOpacity, TextInput, Text, Dimensions } from "react-native";
+import { View, SafeAreaView, TouchableOpacity, TextInput, Text, Dimensions, StyleSheet } from "react-native";
 import Multiply from '../assets/Icons/Multiply.svg';
 import Location from '../assets/Icons/Location.svg';
 import PromtBtn from "../components/PromtBtn";
 
 const screenWidth = Dimensions.get('window').width;
+
 const statusMessages = [
     {id: 1, status: '독서'}, 
     {id: 2, status: '공부'},
@@ -39,22 +40,26 @@ const locationMessages = [
 function EditStoryView({navigation}) {
     const [activeStatusButton, setActiveStatusButton] = useState(null);
     const [activeLocationButton, setActiveLocationButton] = useState(null);
+    const [selectedStatus, setSelectedStatus] = useState('하는');
+    const [selectedLocation, setSelectedLocation] = useState('어딘가');
 
     const onClickApplyButton = () => {
         console.log("완료 버튼 클릭")
     }
 
-    const handleActiveStatusButton = (status) => {
-        setActiveStatusButton(status === activeStatusButton ? null : status);
+    const handleActiveStatusButton = (item) => {
+        setActiveStatusButton(item.id === activeStatusButton ? null : item.id);
+        setSelectedStatus(item.status === selectedStatus ? '하는' : item.status)
     };
 
-    const handleActiveLocationButton = (location) => {
-        setActiveLocationButton(location === activeLocationButton ? null : location);
+    const handleActiveLocationButton = (item) => {
+        setActiveLocationButton(item.id === activeLocationButton ? null : item.id);
+        setSelectedLocation(item.location === selectedLocation ? '어딘가' : item.location)
     }
 
     return(
-        <SafeAreaView style={{flex: 1, backgroundColor: '#000000', alignItems: 'center', paddingHorizontal: 15}}>
-            <View style={{height: 44, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: screenWidth - 30,}}>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
                 <TouchableOpacity onPress={() => navigation.goBack()} style={{position: 'absolute', left: 0}}>
                     <WithLocalSvg
                         width={24}
@@ -71,20 +76,23 @@ function EditStoryView({navigation}) {
 
             <View style={{marginTop: 32, width: screenWidth - 30}}>
                 <View style={{flexDirection: 'row'}}>
-                    <Text style={{fontWeight: 400, fontSize: 32, lineHeight: 38, color: '#F5F5F5'}}>{'{ '}</Text>
-                    <TextInput placeholder="무언가를" placeholderTextColor='#666766' color='#F5F5F5' style={{fontWeight: 500, fontSize: 28, lineHeight: 33}}/>
-                    <Text style={{fontWeight: 400, fontSize: 32, lineHeight: 38, color: '#F5F5F5'}}>{' }'}</Text>
+                    <Text style={styles.curlyBraceText}>{'{ '}</Text>
+                    <TextInput placeholder="무언가를" placeholderTextColor='#666766' color='#F5F5F5' style={styles.actionText}/>
+                    <Text style={styles.curlyBraceText}>{' }'}</Text>
                 </View>
             </View>
 
             <View style={{marginTop: 57, width: screenWidth - 30}}>
-                <Text style={{fontWeight: 500, fontSize: 28, lineHeight: 33, color: '#666766'}}>하는 중</Text>
-                <View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', marginTop: 16, gap: 12}}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Text style={[styles.actionText, {color: selectedStatus == '하는' ? '#666766' : '#F5F5F5'}]}>{selectedStatus}</Text>
+                    <Text style={[styles.actionText, {color: selectedStatus == '하는' ? '#666766' : '#F5F5F5'}]}> 중</Text>
+                </View>
+                <View style={styles.promtButtonContainer}>
                     {statusMessages.map((item, _) => (
                         <PromtBtn
                             key={item.id}
                             title={item.status}
-                            onPress={() => handleActiveStatusButton(item.id)}
+                            onPress={() => handleActiveStatusButton(item)}
                             isSelected={activeStatusButton == item.id}
                         />
                     ))}
@@ -99,14 +107,14 @@ function EditStoryView({navigation}) {
                         asset={Location}
                         color={'#666766'}
                     />
-                    <Text style={{color: '#666766', fontWeight: 500, fontSize: 28, lineHeight: 33}}>어딘가</Text>
+                    <Text style={[styles.actionText, {color: selectedLocation == '어딘가' ? '#666766' : '#F5F5F5'}]}>{selectedLocation}</Text>
                 </View>
-                <View style={{flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', marginTop: 17, gap: 12}}>
+                <View style={styles.promtButtonContainer}>
                     {locationMessages.map((item, _) => (
                         <PromtBtn
                             key={item.id}
                             title={item.location}
-                            onPress={() => handleActiveLocationButton(item.id)}
+                            onPress={() => handleActiveLocationButton(item)}
                             isSelected={activeLocationButton == item.id}
                         />
                     ))}
@@ -118,3 +126,36 @@ function EditStoryView({navigation}) {
 
 export default EditStoryView;
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        backgroundColor: '#000000', 
+        alignItems: 'center', 
+        paddingHorizontal: 15
+    },
+    header: {
+        height: 44, 
+        flexDirection: 'row', 
+        alignItems: 'center', 
+        justifyContent: 'center', 
+        width: screenWidth - 30
+    },
+    curlyBraceText: {
+        fontWeight: 400, 
+        fontSize: 32, 
+        lineHeight: 38, 
+        color: '#F5F5F5'
+    },
+    actionText: {
+        fontWeight: 500, 
+        fontSize: 28, 
+        lineHeight: 33
+    },
+    promtButtonContainer: {
+        flexDirection: 'row', 
+        flexWrap: 'wrap', 
+        alignItems: 'center',
+        marginTop: 17, 
+        gap: 12
+    }
+});
